e2c82c9205c18688587f73f69232668bd25f5445
Henry <henrywl@gmail.com>
2024-06-08 01:30:09 +0100

    re-render after liking post, comment on posts also added

--- src/routes/Main.jsx
+++ src/routes/Main.jsx
@@ -5,6 +5,7 @@ function Main() {
 
     const [feed, setFeed] = useState([])
     const [isLoading, setIsLoading] = useState(true)
+    const [commentText, setCommentText] = useState('')
     // get feed for followed users posts
 
     const auth = useContext(authContext)
@@ -26,6 +27,65 @@ function Main() {
 
         fetchFeed()
     }, [])
+
+
+    const likePostHandler = async(postId) => {
+        console.log('liffihoih')
+        console.log(postId)
+        const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/posts/likePost/${postId}`, {
+            method: "POST",
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+            //   Authorization: "Bearer " + auth.token,
+            },
+            body: JSON.stringify({
+              userID: auth.userId,
+            //   loggedInUserUsername,
+            //   addedUser: uid,
+            //   addedUserUsername,
+              // loggedInUser: authuid
+            }),
+          });
+
+          const data = await response.json()
+
+          console.log(data)
+
+        //   setFeed([data.updatedPost])
+          const updatedFeed = feed.map((post) => {
+            return post._id === postId ? {...data.updatedPost} : post
+          })
+          
+          console.log(updatedFeed, 'updated Feed')
+          //   console.log('first', postId)
+            setFeed(updatedFeed)
+          
+
+    }
+
+    const commentSubmitHandler = async (e, postID) => {
+        e.preventDefault()
+        console.log('first')
+        console.log(postID)
+
+        const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/posts/commentPost/${postID}`, {
+            method: "POST",
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+            //   Authorization: "Bearer " + auth.token,
+            },
+            body: JSON.stringify({
+              userID: auth.userId,
+              text: commentText
+            }),
+          });
+
+          setCommentText('')
+
+    }
+
   return (
     <div>Main
 
@@ -36,7 +96,7 @@ function Main() {
         <div>
             {!isLoading && feed.map((post) => {
                 return (
-                    <div>
+                    <div key={post._id}>
 
                        
                         <div className="card card-compact w-96 bg-base-100 shadow-xl">
@@ -44,11 +104,35 @@ function Main() {
   <div className="card-body">
     <h2 className="card-title">{post.userID.username}</h2>
     <p>{post.status}</p>
+    <p>Likes: {post.likes.length}</p>
     <div className="card-actions justify-end">
-      <button className="btn btn-primary">Like</button>
+      <button className="btn btn-primary" onClick={() => likePostHandler(post._id)}>Like</button>
     </div>
+    <div>
+        Comments: {post.comments.length}
+
+        {post.comments.length !== 0 &&
+        post.comments.map((comment) => {
+            return (
+                <div>
+                    {comment.text}
+                </div>
+            )
+        })        
+        }
+    </div>
+
+    <form onSubmit={(e) => commentSubmitHandler(e, post._id)}>
+
+    <div className="join">
+  <input className="input input-bordered join-item" placeholder="Comment" onChange={(e) => setCommentText(e.target.value)} value={commentText}/>
+  <button className="btn join-item rounded-r-full" type='submit'>Post</button>
+</div>
+
+    </form>
   </div>
 </div>
+
                         </div>
                 )
             })}
