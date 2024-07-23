753510d1421d9be7d66394e26d7ec1d316705e03
Henry <henrywl@gmail.com>
2024-07-23 20:31:34 +0100

    cleaning up code

--- src/components/UserProfilePost.jsx
+++ src/components/UserProfilePost.jsx
@@ -2,14 +2,13 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 function UserProfilePost({ post }) {
-    console.log(post)
-    const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
-    <div className="shadow-xl rounded-lg cursor-pointer" onClick={() => navigate(`/post/${post._id}`)}>
-        {/* change bg to none just have image with no padding */}
-        {/* flex center or padding so all aligned on full screen */}
+    <div
+      className="shadow-xl rounded-lg cursor-pointer"
+      onClick={() => navigate(`/post/${post._id}`)}
+    >
       <img
-        // src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
         src={post.imageURL}
         alt="Shoes"
         className="w-96 h-96 rounded-lg shadow-sm"
